58426268ada11c44fa1e62001d0122f55ec125e1
Charles Lu <kuanminglu0529@gmail.com>
2019-11-10 01:30:08 -0500

    imported celerx to index.js. created reset game function and end game function to integrate with celerx

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import seed from 'random-seed';
+import celerx from './js/celerx';
+
 var express=require('express');
 var app=express();
 var http = require('http').Server(app);
@@ -13,3 +16,13 @@ app.get('/',function(req,res)
 http.listen(process.env.PORT || 5000, function() {
   console.log('listening on port 5000 or server default');
 });
+
+function resetGame(match) {
+  var match = celerx.getMatch();
+seed(match && match.sharedRandomSeed, { global: true });
+celerx.start();
+}
+
+function endGame() {
+  celerx.submitScore(STATE.score);
+}
